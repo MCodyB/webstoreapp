62542477cf603a30a3e19ee3a7a47c1ec5659f47
Cody+Ke <Cody+Ke@appacademy.io>
2013-05-13 13:31:30 -0400

    Sign in complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 WebstoreApp::Application.routes.draw do
   root to: "users#new"
 
+  resource :session, only: [:new, :create, :destroy]
   resources :users
+
   match "/signup", to: "users#new"
+  match "/signin", to: "sessions#new"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130513150011) do
+ActiveRecord::Schema.define(:version => 20130513170707) do
 
   create_table "users", :force => true do |t|
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "email"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
