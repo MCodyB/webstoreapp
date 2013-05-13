13aea8260fe0b1de7c39b4bd6dd3b1dd3e958e5b
Cody+Ke <Cody+Ke@appacademy.io>
2013-05-13 11:08:48 -0400

    First commit, user db done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebstoreApp::Application.config.secret_token = 'ba26b582c590a17a0e6659fb5a4727dde5909db80f6d486d8b767c050271531d0e992a7ffa789946c40ad920a3da5f43ff18079af28a37dca1368752c4a076f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WebstoreApp::Application.config.session_store :cookie_store, key: '_WebstoreApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WebstoreApp::Application.config.session_store :active_record_store
