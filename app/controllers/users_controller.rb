61a80c394e4cc806ff7aa13120ed78d2c5cd0071
Cody+Ke <Cody+Ke@appacademy.io>
2013-05-13 12:00:01 -0400

    Sign up finished, email check and password validations work

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to our Webstore!"
+      redirect_to user_url(@user.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
